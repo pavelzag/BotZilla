b09ca1f3ab25da21a45046c40a6980f38bfdc073
Pavel <zagalsky@gmail.com>
2016-12-17 23:11:30 +0200

    Status fixes

--- bugzilla_call.py
+++ bugzilla_call.py
@@ -25,8 +25,9 @@ def extract_default(updates, type):
     cut_string = full_text_string.split(type + ":", 1)[1].split(" ")[0]
     if cut_string == '':
         cut_string = full_text_string.split(type + ": ", 1)[1].split(" ")[0]
-    if '_' in cut_string:
-        cut_string = cut_string.replace("_", " ")
+    if "on_qa" not in cut_string and "on_qa" not in cut_string:
+        if '_' in cut_string:
+            cut_string = cut_string.replace("_", " ")
     return cut_string
 
 
