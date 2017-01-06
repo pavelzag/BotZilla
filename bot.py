000b8eb82e1040657277daabafcb595a7625588f
Pavel <zagalsky@gmail.com>
2017-01-07 00:19:49 +0200

    heroku fix

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 import sys
 import logging
 import telegram
@@ -48,7 +49,7 @@ def extract_user_to_register(update):
 def worker(bot):
     global update_id
     for update in bot.getUpdates(offset=update_id, timeout=10):
-        if is_registration(update):
+        if is_registration(update) or 'DYNO' not in os.environ:
             print('registration')
             user_name = extract_user_to_register(update)
             result = dbconnector.add_user(update.message.from_user.id, user_name)
