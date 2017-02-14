190ec10bb483fda4b252c3336427e0fb47ea63c6
Pavel <zagalsky@gmail.com>
2017-02-14 08:15:27 +0200

    debugg

--- bugzilla_call.py
+++ bugzilla_call.py
@@ -89,9 +89,9 @@ def query_params(updates):
     bugzilla_status = extract_status(updates)
     bugzilla_assigned_to = extract_assigned_to(updates)
     bugzilla_product = extract_product(updates)
-    print('query params bugzilla product was ' + bugzilla_product)
+    print('query params bugzilla product was ' + str(bugzilla_product))
     bugzilla_product = normalizer.normalize_product_new(bugzilla_product)
-    print('query params after normalization was bugzilla product was ' + bugzilla_product)
+    print('query params after normalization was bugzilla product was ' + str(bugzilla_product))
     bugzilla_component = extract_component(updates)
     bugzilla_component = normalizer.normalize_component_new(bugzilla_component, bugzilla_product)
     return bugzilla_user, bugzilla_status, bugzilla_assigned_to, bugzilla_component, bugzilla_product
